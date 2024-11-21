28341a1a71625da13d89b80a2f2d17db7ad8eb57
Ropler6 <robertmercea@gmail.com>
2024-11-21 22:09:37 +0200

    Init repo
    Signed-off-by: Ropler6 <robertmercea@gmail.com>

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,36 @@
+<script>
+  import "/src/style.css"
+</script>
+
+<div class="header">
+  <div class="recipe-buttons">
+    <img src="/static/logo.png" alt="site logo">
+    <a class="page-link" href="/recipes">Recipes</a>
+    <a class="page-link" href="/addrecipe">Add recipe</a>
+  </div>
+
+  <div class="accounts">
+    <a class="page-link" href="/login">Login</a>
+    <a class="page-link" href="/register">Register</a>
+  </div>
+</div>
+
+
+<style>
+  .header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: var(--size-m) var(--size-xxl);
+    background-color: var(--primary-color);
+    border-radius: 0 0 var(--size-m) var(--size-m);
+    box-shadow: 5px 1px 10px 5px black;
+  }
+
+  .recipe-buttons, .accounts {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: var(--size-l);
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import Header from "$lib/components/Header.svelte";
+
+  //TBA
+  let recipes: string[] = [];
+</script>
+
+
+<Header/>
+<div class="big-logo"></div>
+<div class="recipes">
+  <h2>Top rated recipes</h2>
+
+  <div class="recipe-list">
+    {#each recipes as recipe}
+        <div>{recipe}</div> 
+    {/each}
+  </div>
+</div>
+
+
+<style>
+  .recipes {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .recipe-list {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .big-logo {
+    background: center / contain no-repeat url("");
+    margin: var(--size-m);
+    width: 100vw;
+    height: 300px;
+  }
+</style>
