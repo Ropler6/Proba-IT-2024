1628bb98b31d463a4a808a3eb1edce971ed2bdcc
Ropler6 <robertmercea@gmail.com>
2024-11-22 10:39:53 +0200

    Add mock data and style for Recipe
    Signed-off-by: Ropler6 <robertmercea@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
   import Header from "$lib/components/Header.svelte";
+  import Recipe from "$lib/components/Recipe.svelte";
 
   //TBA
-  let recipes: string[] = [];
+  let recipes: string[] = ["Pateu"];
 </script>
 
 
 <Header/>
 <div class="big-logo"></div>
+
 <div class="recipes">
   <h2>Top rated recipes</h2>
 
   <div class="recipe-list">
     {#each recipes as recipe}
-        <div>{recipe}</div> 
+      <Recipe recipeName={"Pateu"} author={"Student"} rating={5}/>
     {/each}
   </div>
 </div>
@@ -29,6 +31,7 @@
   .recipe-list {
     display: flex;
     flex-direction: row;
+    gap: var(--size-xxl);
   }
 
   .big-logo {
