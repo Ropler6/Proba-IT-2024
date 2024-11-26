3f0eed18c522d2e625fd52239a3eef5b4254de2a
Ropler6 <robertmercea@gmail.com>
2024-11-26 17:40:12 +0200

    /+page.svelte: Partially fix Contact us section
    Signed-off-by: Ropler6 <robertmercea@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,11 +25,10 @@
 <div class="contact">
   <h2>Contact us</h2>
   <form action="">
-    <input class="input-field" type="text" name="first-name" placeholder="First name">
-    <input class="input-field" type="text" name="last-name" placeholder="Last name">
-    <input class="input-field" type="text" name="email" placeholder="Email">
-    <input class="input-field" type="text" name="message" placeholder="Message"
-           style:height="150px">
+    <input type="text" name="first-name" placeholder="First name">
+    <input type="text" name="last-name" placeholder="Last name">
+    <input type="text" name="email" placeholder="Email">
+    <input type="text" name="message" placeholder="Message">
   </form>
 </div>
 
@@ -56,17 +55,36 @@
     height: 300px;
   }
 
+  /* To be fixed */
   .contact {
     padding: var(--size-l);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
+  /* To be fixed */
   form {
     display: flex;
     flex-direction: column;
     gap: var(--size-l);
     padding: var(--size-l);
-    height: 50vh;
+    height: 25vh;
+    width: 75%;
     flex-wrap: wrap;
+  }
+
+  input {
+    color: var(--secondary-color);
+    text-decoration: var(--secondary-color);
+    border: 2px solid var(--secondary-color);
+  }
+
+  /* To be fixed */
+  input[name="message"] {
+    width: 40%;
+    flex: 1;
+    text-align: start;
   }
 
   .contact > h2 {
