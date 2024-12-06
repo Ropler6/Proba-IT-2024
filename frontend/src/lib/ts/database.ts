634794cbb373d10838c0499f47b53ad501beb32d
Ropler6 <robertmercea@gmail.com>
2024-12-06 13:54:40 +0200

    database.ts: Add 'add' function
    Signed-off-by: Ropler6 <robertmercea@gmail.com>

--- frontend/src/lib/ts/database.ts
+++ frontend/src/lib/ts/database.ts
@@ -10,7 +10,13 @@ export const URL = `http://localhost:${PORT}`;
 export const getAll = async<T>(route: string) => {
   const response = await axios.get(`${URL}/${route}`); 
 
-  if (response.status !== 200) throw new Error("Could not fetch data from ${route}");
+  if (response.status !== 200) throw new Error(`Could not fetch data from ${route}`);
 
   return response.data as T[];
+}
+
+export const add = async<T>(route: string, data: T) => {
+  const response = await axios.post(`${URL}/${route}`,(data));
+
+  if (response.status !== 200) throw new Error(`Could not add data to ${route}`);
 }
